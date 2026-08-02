d3f148329b82b292e165b38643a076309be2390a
alibekoffumid <alibekoffumid@antigravity.ai>
2026-08-03 02:46:58 +0400

    build(vite): setup MPA builds using BUILD_MODULE env var

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,29 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
+const moduleToBuild = process.env.BUILD_MODULE;
+
+let buildConfig = {
+  rollupOptions: {
+    input: {
+      main: resolve(__dirname, 'index.html'),
+      warehouse: resolve(__dirname, 'warehouse.html'),
+    }
+  }
+};
+
+if (moduleToBuild) {
+  buildConfig = {
+    outDir: `dist/${moduleToBuild}`,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        [moduleToBuild]: resolve(__dirname, `${moduleToBuild}.html`),
+      }
+    }
+  };
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -10,13 +33,6 @@ export default defineConfig({
     host: true,
     allowedHosts: true,
   },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        warehouse: resolve(__dirname, 'warehouse.html'),
-      }
-    }
-  }
+  build: buildConfig
 })
 
